09af2c4fe89469eb362379829e3fec40bb6c50d6
RedDabbler <redgeeting+oschina@gmail.com>
2018-07-26 20:49:41 +0800

    add BaseTest

---
+++ springboot-java/src/test/java/com/redDabbler/template/springboot/BaseTest.java
@@ -0,0 +1,20 @@
+package com.redDabbler.template.springboot;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("dev")
+public class BaseTest {
+    public static final Logger LOGGER = LoggerFactory.getLogger(BaseTest.class);
+
+}

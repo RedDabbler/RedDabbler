8ff5913c615674e1d793f7ee16aa0181bfce9780
RedDabbler <redgeeting+oschina@gmail.com>
2019-02-22 11:35:07 +0800

    modify spring-boot template

--- springboot-java/src/main/java/com/redDabbler/template/springboot/Application.java
+++ springboot-java/src/main/java/com/redDabbler/template/springboot/Application.java
@@ -1,12 +1,10 @@
 package com.redDabbler.template.springboot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 @ServletComponentScan
 public class Application {
     public static void main(String[] args) {
